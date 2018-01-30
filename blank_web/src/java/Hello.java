dd671d3d80ad2de5a844f8364c8cfdbfe7c4ab16
unknown <gcvolk@zoho.com>
2018-01-30 12:26:20 +0300

    Hello World

---
+++ blank_web/src/java/Hello.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mary
+ */
+public class Hello {
+ private String name;
+ public Hello()
+ {
+ name=null;
+ }
+ public String getName()
+ {
+ return name;
+  }
+ public void setName()
+ {
+     this.name=name;
+ }
+}
---
+++ java_mv/src/main/java/com/mycompany/java_mv/hello.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.java_mv;
+
+/**
+ *
+ * @author Mary
+ */
+public class hello {
+  public static void main(String[] args) {
+        
+    System.out.println("Hello World!");     
+    }  
+
+}
